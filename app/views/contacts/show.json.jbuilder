a1217d8b9de27ad723da0d6d49989e80136bcb2b
ThomasDebek <tom.debek@gmail.com>
2015-03-24 20:38:42 +0100

    Dodajmy teraz formularz kontaktowy

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :id, :name, :surname, :phone, :email, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  resources :contacts
+
   namespace :admin do
     resources :photos
   end
