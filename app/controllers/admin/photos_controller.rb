d95c9ddfdb72bf0d91ce26c570787cfd77a666bf
ThomasDebek <tom.debek@gmail.com>
2015-05-06 21:26:21 +0200

    Ograniczenie dostepu

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PhotosController < ApplicationController
   before_action :set_admin_photo, only: [:show, :edit, :update, :destroy]
-  before_filter :require_login, only: [:show, :edit, :update, :destroy]
+  before_filter :require_login
 
   def index
     @admin_photos = Admin::Photo.all
